7f285c1fa2e330d8af9a328b406edc555961375f
zhaolei <zhaolei7390145@qq.com>
2016-01-20 10:02:49 +0800

    update

---
+++ 数据持久化存储/SeetingBundles/SeetingBundles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SeetingBundles
+//
+//  Created by 赵磊 on 16/1/19.
+//  Copyright © 2016年 赵磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
